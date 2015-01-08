0887133bfc43ea5deab7ff61478e8a7dc18a5567
Dan <danpbirnbaum@gmail.com>
2015-01-08 15:10:10 -0500

    modified parse-table.R

--- read_counts/parse-table.R
+++ read_counts/parse-table.R
@@ -1,6 +1,5 @@
-data <- read.delim('appris-principal-transcripts_pos_only_no_readthrough.table', header=TRUE, sep="\t", check.names=FALSE)
-# library(edgeR)
-# isexpr <- cpm(data)>50
+args <- commandArgs(trailingOnly=TRUE)
+data <- read.delim(args[1], header=TRUE, sep="\t", check.names=FALSE)
 isexpr <- data>100
 samples <- colnames(data)
 genes <- rownames(data)
@@ -8,7 +7,5 @@ for (i in 1:length(samples)) {
 sample <- samples[i]
 expr <- genes[isexpr[,sample]]
 line <- c(sample, expr)
-# print(line)
-# print(length(line))	
 write(line, file="../metagene/appris_principal/appris_principal_expressed_by_sample.pos_only.no_readthrough.txt", append=TRUE, sep="\t", ncolumns=length(line))
 }
